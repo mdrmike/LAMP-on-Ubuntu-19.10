51eaf101861dbe14ed941e26d30b70b639eed62b
mike stewart <stewart@MediaDoneRight.com>
2020-05-26 17:18:01 -0700

    :wrench: ADD conditional logic to create  logfile. Credit to logfile source.

--- lampon2004.sh
+++ lampon2004.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-exec   > >(tee -ia bash.log)
-exec  2> >(tee -ia bash.log >& 2)
-exec 19> /root/install.log
-export BASH_XTRACEFD="19"
-set -x
+if [ "${SSDEBUG,,}" == "yes" ]; then
+  # CREATE LOGFILE, 
+  #   based on https://askubuntu.com/a/1001404/139249
+  exec   > >(tee -ia bash.log)
+  exec  2> >(tee -ia bash.log >& 2)
+  exec 19> /root/install.log
+  export BASH_XTRACEFD="19"
+  set -x
+fi
 
 # INSTALL UPDATES
 apt -y update
