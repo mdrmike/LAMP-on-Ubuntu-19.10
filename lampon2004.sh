103169911c99bff7d84e050a3cfa9af0ae779dab
mike stewart <stewart@MediaDoneRight.com>
2020-06-08 15:46:48 -0700

    :bug: FIX zsh install try two

--- lampon2004.sh
+++ lampon2004.sh
@@ -236,8 +236,12 @@ fi
 if [ "${SSZSH,,}" = "yes" ]; then
   apt-get -y install zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended 
+  while [ ! -f "/root/.zshrc" ] && [ ! -d "/root/.oh-my-zsh" ]                      # wait until oh-my-zsh is installed
+  do
+    sleep 3 # or less like 0.2
+  done
   sed -i 's|ZSH_THEME=".*"|ZSH_THEME="rkj-repos"|g' ~/.zshrc
-  sed -i "s|root|$SSUSER|g" ~/.zshrc
+  sed -i 's|ZSH=\"*\.oh-my-zsh\"|ZSH=\"$HOME/.oh-my-zsh\"|g' ~/.zshrc
   mv .oh-my-zsh /etc/skel/
   mv .z* /etc/skel/
 fi
